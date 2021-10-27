f14ee56c8c3e4d8b5a9a5ae9b8daae3a2170b908
AkinAguda <lordtitan7@gmail.com>
2021-10-27 12:18:04 +0100

    fluid sim kinda works

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import Renderer from "./renderer";
 import { Fluid, FluidConfig } from "./fluid";
 
-const fluidConfig = new FluidConfig(128, 20, 0.1);
+const fluidConfig = new FluidConfig(128, 1, 0.1);
 const fluid = new Fluid(fluidConfig);
 let renderer = new Renderer(fluid);
 renderer.start();
