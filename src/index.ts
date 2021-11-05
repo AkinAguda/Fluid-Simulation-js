b2abbeb6bacadfbe49aaa2f0d584d230a89f4744
AkinAguda <lordtitan7@gmail.com>
2021-11-05 15:20:48 +0100

    adjusted config a bit. Need to add UI for users to adjust config manually

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import Renderer from "./renderer";
 import { Fluid, FluidConfig } from "./fluid";
 
-const fluidConfig = new FluidConfig(100, 1, 0.3);
+const fluidConfig = new FluidConfig(170, 0.3, 0.7);
 const fluid = new Fluid(fluidConfig);
 let renderer = new Renderer(fluid);
 renderer.start();
