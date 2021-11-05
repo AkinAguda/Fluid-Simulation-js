303530b4d72e75a96a8614e6dd318fcb1093cd64
AkinAguda <lordtitan7@gmail.com>
2021-11-05 15:23:35 +0100

    updated default config

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import Renderer from "./renderer";
 import { Fluid, FluidConfig } from "./fluid";
 
-const fluidConfig = new FluidConfig(170, 0.3, 0.7);
+const fluidConfig = new FluidConfig(140, 0.8, 0.6);
 const fluid = new Fluid(fluidConfig);
 let renderer = new Renderer(fluid);
 renderer.start();
